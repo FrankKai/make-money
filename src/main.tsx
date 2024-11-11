aaf7df39df0558a0700efbf47ef58c6807ec32b7
FrankKai <gaokai20100801@gmail.com>
2024-11-11 16:38:10 +0800

    app keep alive

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,6 @@
-import React from "react";
+import React, { useState } from "react";
 import ReactDOM from "react-dom/client";
-import { BrowserRouter as Router, Route, Routes, Link } from "react-router-dom";
+import { BrowserRouter as Router, Link, useLocation } from "react-router-dom";
 import MakeMoney from "./pages/makeMoney";
 import Rank from "./pages/rank";
 import "./App.css";
@@ -21,13 +21,33 @@ function Navigation() {
 }
 
 function App() {
+  const location = useLocation();
+  const [activeComponents, setActiveComponents] = useState<Record<string, any>>(
+    {}
+  );
+
+  React.useEffect(() => {
+    setActiveComponents((prev) => ({
+      ...prev,
+      [location.pathname]: true,
+    }));
+  }, [location]);
+
+  const renderComponent = (path: string, Component: React.FC) => {
+    return activeComponents[path] ? (
+      <div style={{ display: location.pathname === path ? "block" : "none" }}>
+        <Component />
+      </div>
+    ) : null;
+  };
+
   return (
     <div>
       <Navigation />
-      <Routes>
-        <Route path="/" element={<MakeMoney />} />
-        <Route path="/rank" element={<Rank />} />
-      </Routes>
+      <div>
+        {renderComponent("/", MakeMoney)}
+        {renderComponent("/rank", Rank)}
+      </div>
     </div>
   );
 }
